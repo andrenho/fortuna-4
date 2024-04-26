a2c87cf7d05100d2f1bf7fa8bb14bc5e7f55a36e
Andre Wagner <WagnerAndre@JohnDeere.com>
2024-04-26 17:18:43 -0500

    .

--- firmware/io/src/serial.cc
+++ firmware/io/src/serial.cc
@@ -14,14 +14,20 @@ static void input_cb([[maybe_unused]] void* data)
 void init()
 {
     stdio_usb_init();
-    stdio_set_chars_available_callback(input_cb, nullptr);
+    // stdio_set_chars_available_callback(input_cb, nullptr);
 }
 
 uint8_t read()
 {
+    int current = getchar_timeout_us(0);
+    if (current != 0 && current != PICO_ERROR_TIMEOUT)
+        return current;
+    return 0;
+    /*
     char data = last_char;
     last_char = 0x0;
     return data;
+     */
 }
 
 void write(uint8_t data)
